c15c3933a8de7e39c5cdd764eba25242b6f4cf06
Erica Cheong <erica.clw@gmail.com>
2018-02-10 00:26:30 +0800

    Update config

--- config/prod.exs
+++ config/prod.exs
@@ -24,7 +24,7 @@ config :todo, TodoWeb.Endpoint,
 config :logger, level: :info
 
 # Configure your database
-config :todo, TodoWeb.Repo,
+config :todo, Todo.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
